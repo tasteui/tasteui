f15958dde3868fc6eaa324d2a43b5e36455acde2
AJ Meireles <alvaro.meireles@live.com>
2025-01-01 02:20:23 -0300

    wip

--- src/resources/views/components/layout/sidebar/index.blade.php
+++ src/resources/views/components/layout/sidebar/index.blade.php
@@ -5,7 +5,8 @@
          x-transition:leave="transition-opacity ease-linear duration-300"
          x-transition:leave-start="opacity-100"
          x-transition:leave-end="opacity-0"
-         class="fixed inset-0 bg-gray-900/80" x-show="tallStackUiMenuMobile"></div>
+         class="fixed inset-0 bg-gray-900/80"
+         x-show="tallStackUiMenuMobile"></div>
     <div class="fixed inset-0 flex">
         <div x-transition:enter="transition ease-in-out duration-300 transform"
              x-transition:enter-start="-translate-x-full"
@@ -13,9 +14,9 @@
              x-transition:leave="transition ease-in-out duration-300 transform"
              x-transition:leave-start="translate-x-0"
              x-transition:leave-end="-translate-x-full"
-             class="relative mr-16 flex w-full max-w-xs flex-1" x-show="tallStackUiMenuMobile">
-            <div x-on:click.outside="tallStackUiMenuMobile = false"
-                 x-show="tallStackUiMenuMobile"
+             class="relative mr-16 flex w-full max-w-xs flex-1"
+             x-show="tallStackUiMenuMobile">
+            <div x-show="tallStackUiMenuMobile"
                  x-transition:enter="ease-in-out duration-300"
                  x-transition:enter-start="opacity-0"
                  x-transition:enter-end="opacity-100"
@@ -30,13 +31,13 @@
                     </svg>
                 </button>
             </div>
-            <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-6 pb-4" x-on:click.outside="tallStackUiMenuMobile = false">
+            <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-2 pb-4" x-on:click.outside="tallStackUiMenuMobile = false">
                 @if ($brand)
                     {{ $brand }}
                 @endif
                 <div class="mt-10 flex h-16 shrink-0 items-center">
                     <nav class="flex h-16 flex-1 flex-col">
-                        <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                        <ul role="list" class="flex flex-1 flex-col gap-y-2">
                             {{ $slot }}
                         </ul>
                     </nav>
@@ -46,13 +47,13 @@
     </div>
 </div>
 <div class="hidden md:fixed md:inset-y-0 md:z-50 md:flex md:w-72 md:flex-col">
-    <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 pb-4">
+    <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-2 pb-4">
         @if ($brand)
             {{ $brand }}
         @endif
         <div class="mt-10 flex h-16 shrink-0 items-center">
             <nav class="flex h-16 flex-1 flex-col">
-                <ul role="list" class="flex flex-1 flex-col gap-y-7">
+                <ul role="list" class="flex flex-1 flex-col">
                     {{ $slot }}
                 </ul>
             </nav>
--- src/resources/views/components/layout/sidebar/item.blade.php
+++ src/resources/views/components/layout/sidebar/item.blade.php
@@ -1,46 +1,34 @@
-{{--<li>--}}
-{{--    <a @if ($route) href="{{ $route }}" @endif {{ $attributes->class([--}}
-{{--            'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold transition',--}}
-{{--            'text-primary hover:bg-primary-100' => !$activated,--}}
-{{--            'bg-primary text-white' => $activated,--}}
-{{--        ]) }}>--}}
-{{--        {{ $label ?? $slot }}--}}
-{{--    </a>--}}
-{{--</li>--}}
-
 @if ($grouped)
     <li x-data="{ show : false }">
-        <div>
-            <button x-on:click="show = !show"
-                    type="button"
-                    class="flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm/6 font-semibold text-gray-700 hover:bg-gray-50">
-                @if ($icon instanceof \Illuminate\View\ComponentSlot)
-                    {{ $icon }}
-                @elseif ($icon)
-                    <x-dynamic-component :component="TallStackUi::prefix('icon')"
-                                         :icon="TallStackUi::icon($icon)"
-                                         internal
-                                         class="w-6 h-6 shrink-0 text-gray-400" />
-                @endif
-                {{ $label }}
-                @if ($collapseIcon instanceof \Illuminate\View\ComponentSlot)
-                    {{ $collapseIcon }}
-                @else
+        <button x-on:click="show = !show"
+                type="button"
+                class="flex w-full items-center gap-x-3 rounded-md p-2 text-left text-sm/6 font-semibold text-gray-700 hover:bg-gray-100 transition-all">
+            @if ($icon instanceof \Illuminate\View\ComponentSlot)
+                {{ $icon }}
+            @elseif ($icon)
+                <x-dynamic-component :component="TallStackUi::prefix('icon')"
+                                     :icon="TallStackUi::icon($icon)"
+                                     internal
+                                     class="w-6 h-6 shrink-0 text-gray-400" />
+            @endif
+            {{ $label }}
+            @if ($collapseIcon instanceof \Illuminate\View\ComponentSlot)
+                {{ $collapseIcon }}
+            @else
                 <x-tallstack-ui::icon.generic.chevron-down class="ml-auto w-4 h-4 shrink-0 text-gray-400 transition-all"
-                                                            x-bind:class="{
+                                                           x-bind:class="{
                                                                 'rotate-180 text-gray-500': show,
                                                                 'text-gray-400': !show
-                                                            }" />
-                @endif
-            </button>
-            <ul x-show="show" class="mt-1 px-2">
-                {{ $slot }}
-            </ul>
-        </div>
+                                                           }" />
+            @endif
+        </button>
+        <ul x-show="show" class="mt-1 px-2">
+            {{ $slot }}
+        </ul>
     </li>
 @else
     <li>
-        <a @if ($route) href="{{ $route }}" @endif class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-700 hover:bg-gray-50 cursor-pointer">
+        <a @if ($route) href="{{ $route }}" @endif class="group flex gap-x-3 rounded-md p-2 text-sm/6 font-semibold text-gray-700 hover:bg-gray-100 transition-all">
             @if ($icon instanceof \Illuminate\View\ComponentSlot)
                 {{ $icon }}
             @elseif ($icon)
