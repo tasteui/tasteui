f7164247a23df28b802ac029410cb7192d215bff
AJ Meireles <alvaro.meireles@live.com>
2025-01-02 00:28:10 -0300

    wip

--- src/View/Components/Layout/SideBar/Index.php
+++ src/View/Components/Layout/SideBar/Index.php
@@ -12,11 +12,8 @@ use TallStackUi\TallStackUiComponent;
 #[SoftPersonalization('layout.side-bar')]
 class Index extends TallStackUiComponent implements Personalization
 {
-    public function __construct(
-        public ?ComponentSlot $brand = null,
-        public ?bool $desktopOnly = null,
-        public ?bool $mobileOnly = null,
-    ) {
+    public function __construct(public ?ComponentSlot $brand = null)
+    {
         //
     }
 
@@ -27,6 +24,32 @@ class Index extends TallStackUiComponent implements Personalization
 
     public function personalization(): array
     {
-        return Arr::dot([]);
+        return Arr::dot([
+            'mobile' => [
+                'wrapper' => [
+                    'first' => 'relative z-50 md:hidden',
+                    'second' => 'fixed inset-0 flex',
+                    'third' => 'relative mr-16 flex w-full max-w-xs flex-1',
+                    'fourth' => 'flex grow flex-col gap-y-5 overflow-y-auto bg-white px-2 pb-4',
+                    'fifth' => 'mt-10 flex h-16 shrink-0 items-center',
+                    'sixth' => 'flex h-16 flex-1 flex-col',
+                    'seventh' => 'flex flex-1 flex-col gap-y-2',
+                ],
+                'button' => [
+                    'wrapper' => 'absolute left-full top-0 flex w-16 justify-center pt-5',
+                    'size' => 'w-6 h-6 text-white',
+                    'icon' => 'x-mark',
+                ],
+            ],
+            'desktop' => [
+                'wrapper' => [
+                    'first' => 'hidden md:fixed md:inset-y-0 md:z-50 md:flex md:w-72 md:flex-col',
+                    'second' => 'flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-2 pb-4',
+                    'third' => 'mt-10 flex h-16 shrink-0 items-center',
+                    'fourth' => 'flex h-16 flex-1 flex-col',
+                    'fifth' => 'flex flex-1 flex-col',
+                ],
+            ],
+        ]);
     }
 }
--- src/resources/views/components/layout/sidebar/index.blade.php
+++ src/resources/views/components/layout/sidebar/index.blade.php
@@ -1,4 +1,8 @@
-<div class="relative z-50 md:hidden" role="dialog" aria-modal="true" x-show="tallStackUiMenuMobile">
+@php
+    $personalize = $classes();
+@endphp
+
+<div class="{{ $personalize['mobile.wrapper.first'] }}" role="dialog" aria-modal="true" x-show="tallStackUiMenuMobile">
     <div x-transition:enter="transition-opacity ease-linear duration-300"
          x-transition:enter-start="opacity-0"
          x-transition:enter-end="opacity-100"
@@ -7,15 +11,16 @@
          x-transition:leave-end="opacity-0"
          class="fixed inset-0 bg-gray-900/80"
          x-show="tallStackUiMenuMobile"></div>
-    <div class="fixed inset-0 flex">
+    <div class="{{ $personalize['mobile.wrapper.second'] }}">
         <div x-transition:enter="transition ease-in-out duration-300 transform"
              x-transition:enter-start="-translate-x-full"
              x-transition:enter-end="translate-x-0"
              x-transition:leave="transition ease-in-out duration-300 transform"
              x-transition:leave-start="translate-x-0"
              x-transition:leave-end="-translate-x-full"
-             class="relative mr-16 flex w-full max-w-xs flex-1"
+             class="{{ $personalize['mobile.wrapper.third'] }}"
              x-show="tallStackUiMenuMobile">
+            @if (filled($personalize['mobile.button.icon']))
             <div x-show="tallStackUiMenuMobile"
                  x-transition:enter="ease-in-out duration-300"
                  x-transition:enter-start="opacity-0"
@@ -23,21 +28,22 @@
                  x-transition:leave="ease-in-out duration-300"
                  x-transition:leave-start="opacity-100"
                  x-transition:leave-end="opacity-0"
-                 class="absolute left-full top-0 flex w-16 justify-center pt-5">
-                <button x-on:click="tallStackUiMenuMobile = false" type="button" class="-m-2.5 p-2.5">
-                    <span class="sr-only">Close sidebar</span>
-                    <svg class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                 class="{{ $personalize['mobile.button.wrapper'] }}">
+                <button x-on:click="tallStackUiMenuMobile = false" type="button">
+                    <x-dynamic-component :component="TallStackUi::prefix('icon')"
+                                         :icon="TallStackUi::icon($personalize['mobile.button.icon'])"
+                                         internal
+                                         class="{{ $personalize['mobile.button.size'] }}" />
                 </button>
             </div>
-            <div class="flex grow flex-col gap-y-5 overflow-y-auto bg-white px-2 pb-4" x-on:click.outside="tallStackUiMenuMobile = false">
+            @endif
+            <div class="{{ $personalize['mobile.wrapper.fourth'] }}" x-on:click.outside="tallStackUiMenuMobile = false">
                 @if ($brand)
                     {{ $brand }}
                 @endif
-                <div class="mt-10 flex h-16 shrink-0 items-center">
-                    <nav class="flex h-16 flex-1 flex-col">
-                        <ul role="list" class="flex flex-1 flex-col gap-y-2">
+                <div class="{{ $personalize['mobile.wrapper.fifth'] }}">
+                    <nav class="{{ $personalize['mobile.wrapper.sixth'] }}">
+                        <ul role="list" class="{{ $personalize['mobile.wrapper.seventh'] }}">
                             {{ $slot }}
                         </ul>
                     </nav>
@@ -46,14 +52,14 @@
         </div>
     </div>
 </div>
-<div class="hidden md:fixed md:inset-y-0 md:z-50 md:flex md:w-72 md:flex-col">
-    <div class="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-2 pb-4">
+<div class="{{ $personalize['desktop.wrapper.first'] }}">
+    <div class="{{ $personalize['desktop.wrapper.second'] }}">
         @if ($brand)
             {{ $brand }}
         @endif
-        <div class="mt-10 flex h-16 shrink-0 items-center">
-            <nav class="flex h-16 flex-1 flex-col">
-                <ul role="list" class="flex flex-1 flex-col">
+        <div class="{{ $personalize['desktop.wrapper.third'] }}">
+            <nav class="{{ $personalize['desktop.wrapper.fourth'] }}">
+                <ul role="list" class="{{ $personalize['desktop.wrapper.fifth'] }}">
                     {{ $slot }}
                 </ul>
             </nav>
