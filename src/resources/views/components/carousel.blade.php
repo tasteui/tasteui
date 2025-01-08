783e2367ae157be4593bc4c81ebaf2ef32cb1db9
AJ Meireles <alvaro.meireles@live.com>
2025-01-08 11:50:28 -0300

    wip

--- src/resources/views/components/carousel.blade.php
+++ src/resources/views/components/carousel.blade.php
@@ -4,26 +4,26 @@
     @if ($header)
         {{ $header }}
     @endif
-
     <div class="relative w-full overflow-hidden">
         @if (!$autoplay)
-            <!-- previous button -->
-            <button type="button" class="absolute left-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-white/40 p-2 text-slate-700 transition hover:bg-white/60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:outline-offset-0 dark:bg-slate-900/40 dark:text-slate-300 dark:hover:bg-slate-900/60 dark:focus-visible:outline-blue-600" aria-label="previous slide" x-on:click="previous()">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pr-0.5" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5 8.25 12l7.5-7.5" />
-                </svg>
+            <button type="button"
+                    class="absolute left-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-white/40 p-2 text-slate-700 transition hover:bg-white/60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:outline-offset-0 dark:bg-slate-900/40 dark:text-slate-300 dark:hover:bg-slate-900/60 dark:focus-visible:outline-blue-600"
+                    x-on:click="previous()">
+                <x-dynamic-component :component="TallStackUi::prefix('icon')"
+                                     :icon="TallStackUi::icon('chevron-left')"
+                                     internal
+                                     class="w-6 h-6 pr-0.5" />
             </button>
-
-            <!-- next button -->
-            <button type="button" class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-white/40 p-2 text-slate-700 transition hover:bg-white/60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:outline-offset-0 dark:bg-slate-900/40 dark:text-slate-300 dark:hover:bg-slate-900/60 dark:focus-visible:outline-blue-600" aria-label="next slide" x-on:click="next()">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke="currentColor" fill="none" stroke-width="3" class="size-5 md:size-6 pl-0.5" aria-hidden="true">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                </svg>
+            <button type="button"
+                    class="absolute right-5 top-1/2 z-20 flex rounded-full -translate-y-1/2 items-center justify-center bg-white/40 p-2 text-slate-700 transition hover:bg-white/60 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-700 active:outline-offset-0 dark:bg-slate-900/40 dark:text-slate-300 dark:hover:bg-slate-900/60 dark:focus-visible:outline-blue-600"
+                    aria-label="next slide"
+                    x-on:click="next()">
+                <x-dynamic-component :component="TallStackUi::prefix('icon')"
+                                     :icon="TallStackUi::icon('chevron-right')"
+                                     internal
+                                     class="w-6 h-6 pl-0.5" />
             </button>
         @endif
-
-        <!-- slides -->
-        <!-- Change min-h-[50svh] to your preferred height size -->
         <div @class([
             'relative w-full',
             'min-h-[50svh]' => is_null($wrapper),
@@ -52,7 +52,6 @@
                 </div>
             </template>
         </div>
-
         @if (!$withoutIndicators)
             <div class="absolute rounded-xl bottom-3 md:bottom-5 left-1/2 z-20 flex -translate-x-1/2 gap-4 md:gap-3 bg-white/75 px-1.5 py-1 md:px-2 dark:bg-slate-900/75" role="group" aria-label="slides">
                 <template x-for="(image, index) in images">
@@ -61,7 +60,6 @@
             </div>
         @endif
     </div>
-
     @if ($footer)
         {{ $footer }}
     @endif
